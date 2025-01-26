18987b4d57efc41a8005efabf22d7797c18f89fb
Isaac King <isaacking7505@gmail.com>
2025-01-26 15:43:06 -0500

    New file!

--- DataTypes.java
+++ DataTypes.java
@@ -1,11 +1,10 @@
 public class DataTypes {
-    
 
     private static void displayValues(String name, int age, double cashOnHand, boolean isStudent) {
 
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
-        System.out.println("Cash On Hand: " + casOnHand);
+        System.out.println("Cash On Hand: " + cashOnHand); 
         System.out.println("Is Student: " + isStudent);
     }
 
@@ -15,8 +14,8 @@ public class DataTypes {
         int age = 25;
         double cashOnHand = 10.25;
         boolean isStudent = false;
-        
 
-        // displayValues(name, age, cashOnHand, isStudent);
+        
+        displayValues(name, age, cashOnHand, isStudent);
     }
 }
--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -1,37 +1,18 @@
 public class TemperatureConverter {
-    
 
     private static void convertToCelsius(double degreesInFahrenheit) {
 
         // #### Assignment Starts Here ####
-
-        /**
-         * 
-         * Please write an "algorithm" below taking the "input parameter" degreesInFarenheit
-         * and converting it to the variable "degreesInCelsius" which will be printed below
-         *  
-         */
-
-        
-
-        
-
-
-
+        double degreesInCelsius = (degreesInFahrenheit - 32) * 5 / 9;
         // #### Assignment Ends Here ####
 
-
         System.out.println(degreesInFahrenheit + " to Celsius is: " + degreesInCelsius);
-
     }
 
     public static void main(String[] args) {
 
         double degreesInFahrenheit = 25;
-        
 
         convertToCelsius(degreesInFahrenheit);
-
-
     }
 }
